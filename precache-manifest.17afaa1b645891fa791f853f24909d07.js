941f804219435861e9f54d914036fc2844327e24
huan6766 <271492388@qq.com>
2021-08-21 18:28:18 +0800

    deploy

---
+++ precache-manifest.17afaa1b645891fa791f853f24909d07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb3d8e55d8bf7967946",
+    "url": "/morney-website/css/app.3af9c0ee.css"
+  },
+  {
+    "revision": "6ffc735b2f496af682414e0f415b91ee",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "beb3d8e55d8bf7967946",
+    "url": "/morney-website/js/app.8c33893e.js"
+  },
+  {
+    "revision": "e7345d0228b320ac0728",
+    "url": "/morney-website/js/chunk-vendors.ced40295.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
